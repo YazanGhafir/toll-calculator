d8d7d488a004a6f2a56c2dac481092b729299ea9
Yazan Ghafir <Yazan.Ghafir@hedinit.com>
2024-08-26 01:51:59 +0200

    First commit

---
+++ src/main/java/org/yazanghafir/tollcalculator/HelloController.java
@@ -0,0 +1,15 @@
+package org.yazanghafir.tollcalculator;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String sayHello() {
+        return "Hello, World!";
+    }
+}
---
+++ src/main/java/org/yazanghafir/tollcalculator/TollCalculatorApplication.java
@@ -0,0 +1,13 @@
+package org.yazanghafir.tollcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TollCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TollCalculatorApplication.class, args);
+    }
+
+}

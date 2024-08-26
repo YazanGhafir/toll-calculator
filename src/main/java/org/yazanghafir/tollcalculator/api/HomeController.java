3a4194996009fc169e69309de53f72f5154b0aa9
Yazan Ghafir <Yazan.Ghafir@hedinit.com>
2024-08-26 10:14:58 +0200

    Added redirecting controller

---
+++ src/main/java/org/yazanghafir/tollcalculator/api/HomeController.java
@@ -0,0 +1,14 @@
+package org.yazanghafir.tollcalculator.api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public RedirectView redirectToSwagger() {
+        return new RedirectView("/swagger-ui/index.html#/toll-calculator-controller/calculateToll");
+    }
+}
